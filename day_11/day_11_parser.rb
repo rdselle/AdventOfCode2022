80eeff8d5e0e787e990005bd7f82d44988002610
Robert Sellers <robertsellers@Roberts-MacBook-Pro.local>
2022-12-11 00:25:33 -0600

    day 11 part 1

--- day_11/day_11_parser.rb
+++ day_11/day_11_parser.rb
@@ -17,39 +17,66 @@ class Day11Parser
         case split[0]
         when "Monkey"
           current_monkey = Monkey.new(split[1].chars[0].to_i)
-          puts "created monkey #{current_monkey.m_id}"
+          # puts "created monkey #{current_monkey.m_id}"
         when "Starting"
           (2..(split.length - 1)).each do |i|
             current_monkey.add_item(split[i].delete_suffix(",").to_i)
           end
         when "Operation:"
           current_monkey.add_operation(split[3], split[4], split[5])
-          puts "monkey #{current_monkey.m_id} operation: #{current_monkey.operation}"
+          # puts "monkey #{current_monkey.m_id} operation: #{current_monkey.operation}"
         when "Test:"
           current_monkey.test = split.last.to_i
-          puts "monkey #{current_monkey.m_id} test: #{current_monkey.test}"
+          # puts "monkey #{current_monkey.m_id} test: #{current_monkey.test}"
         when "If"
           if split[1] == "true:"
             current_monkey.throw_to_true = split.last.to_i
-            puts "monkey #{current_monkey.m_id} throw to true: #{current_monkey.throw_to_true}"
+            # puts "monkey #{current_monkey.m_id} throw to true: #{current_monkey.throw_to_true}"
           else
             current_monkey.throw_to_false = split.last.to_i
-            puts "monkey #{current_monkey.m_id} throw to false: #{current_monkey.throw_to_false}"
+            # puts "monkey #{current_monkey.m_id} throw to false: #{current_monkey.throw_to_false}"
           end
         end
       end
       @monkeys << current_monkey
 
-      @monkeys.each do |monkey|
-        monkey.items.each_with_index do |item, i|
-          puts "monkey #{monkey.m_id} item #{item} operation result: #{monkey.perform_operation(i)}"
+      # @monkeys.each do |monkey|
+      #   monkey.items.each_with_index do |item, i|
+      #     puts "monkey #{monkey.m_id} item #{item} operation result: #{monkey.perform_operation(i)}"
+      #   end
+      # end
+    end
+
+    def perform_rounds
+      #part 1
+      # (1..20).each do |i|
+      #part 2
+      (1..10000).each do |i|
+        @monkeys.each do |monkey|
+          monkey.items.each_with_index do |item, i|
+            inspected_worry = monkey.perform_inspection(i)
+            if inspected_worry % monkey.test == 0
+              @monkeys[monkey.throw_to_true].add_item(inspected_worry)
+            else
+              @monkeys[monkey.throw_to_false].add_item(inspected_worry)
+            end
+          end
+          monkey.items = []
         end
       end
+    end
+
+    def monkey_business
+      @monkeys.sort! { |x, y| y.inspection_count <=> x.inspection_count}
+      @monkeys.each do |monkey|
+        puts "monkey #{monkey.m_id} inspected #{monkey.inspection_count} items"
+      end
+      @monkeys[0].inspection_count * @monkeys[1].inspection_count
     end
 end
 
 class Monkey
-  attr_accessor :m_id, :items, :operation, :test, :throw_to_true, :throw_to_false
+  attr_accessor :m_id, :items, :operation, :test, :throw_to_true, :throw_to_false, :inspection_count
 
   def initialize(m_id)
     @m_id = m_id
@@ -58,10 +85,11 @@ class Monkey
     @test = 0
     @throw_to_true = 0
     @throw_to_false = 0
+    @inspection_count = 0
   end
 
   def add_item(item)
-    puts "adding #{item} to monkey #{@m_id}"
+    # puts "adding #{item} to monkey #{@m_id}"
     @items << item
   end
 
@@ -69,7 +97,8 @@ class Monkey
     @operation = [element_1, element_2, element_3]
   end
 
-  def perform_operation(item_index)
+  def perform_inspection(item_index)
+    @inspection_count += 1
     value = 0
     left = @operation[0] == "old" ? @items[item_index] : @operation[0].to_i
     operator = @operation[1]
@@ -82,9 +111,15 @@ class Monkey
       value = left * right
     end
 
+    #part 1
+    # value / 3
+
+    #part 2
     value
   end
 end
 
 parser = Day11Parser.new
 parser.process_data
+parser.perform_rounds
+puts "total monkey business: #{parser.monkey_business}"
